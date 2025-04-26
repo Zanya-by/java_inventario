18c78270eda190ad40402cfcbbecc28483500c1e
Zany-by <enzoxgamerx@gmail.com>
2025-04-26 10:07:48 -0300

    Entidade Product testada

---
+++ src/inventory/Product.java
@@ -0,0 +1,74 @@
+package inventory;
+
+public class Product {
+    private int itemNumber;
+    private String name;
+    private int qtdInStock;
+    private double price;
+    private boolean active = true;
+    
+    public Product() {}
+    
+    public Product(int number, String name, int qtd, double price)
+    {
+    	this.itemNumber = number;
+    	this.name = name;
+    	this.qtdInStock = qtd;
+    	this.price = price;
+    }
+
+	public int getItemNumber() {
+		return itemNumber;
+	}
+
+	public void setItemNumber(int itemNumber) {
+		this.itemNumber = itemNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getQtdInStock() {
+		return qtdInStock;
+	}
+
+	public void setQtdInStock(int qtdInStock) {
+		this.qtdInStock = qtdInStock;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	public double getInventotyValue()
+	{
+		return this.price * this.qtdInStock;
+	}
+
+	public String toString()
+    {
+    	return "\nItem Number: " + this.itemNumber
+    			+ "\nName:" + this.name
+    			+ "\nQuzntity in stock: " + qtdInStock
+    			+ "\nPrice: " + this.price
+    			+"\nStock Value: " + this.getInventoryValue()
+    			+ "\nStatus: " + (this.active ? "Ativo" : "Descontinuado");
+    } 
+}
---
+++ src/inventory/ProductTester.java
@@ -0,0 +1,30 @@
+package inventory;
+import java.util.Scanner;
+
+public class ProductTester {
+
+	
+   public static void main(String[] args) {
+	   Scanner in = new Scanner(System.in);
+	   int tempNumber;
+	   String tempName;
+	   int tempQtd;
+	   double tempPrice;
+	   
+	   System.out.println("\n\n Digite o nome do Produto: ");
+	   tempName = in.nextLine();
+	   System.out.println("Digite a quantidade do estoque: ");
+	   tempQtd = in.nextInt();
+	   System.out.println("Digite o preço: ");
+	   tempPrice = in.nextDouble();
+	   System.out.println("Digite o Indentificador único: ");
+	   tempNumber = in.nextInt();
+	   
+	   in.close();
+       Product p = new Product(tempNumber, tempName, tempQtd, tempPrice); 
+       p.setActive(false);
+       System.out.println(p);
+       
+   }
+}
+    		
